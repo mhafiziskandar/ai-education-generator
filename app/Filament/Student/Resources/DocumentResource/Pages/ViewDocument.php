85ded22450b788d5d8bc0968ad410a74f826304c
mhafiziskandar <mhafiziskandar.dev@gmail.com>
2024-11-17 06:59:37 +0800

    add document resources for student

---
+++ app/Filament/Student/Resources/DocumentResource/Pages/ViewDocument.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Student\Resources\DocumentResource\Pages;
+
+use App\Filament\Student\Resources\DocumentResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewDocument extends ViewRecord
+{
+    protected static string $resource = DocumentResource::class;
+}
