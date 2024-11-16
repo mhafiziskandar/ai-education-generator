b8edcc818479a005e7000e509a4da88c31c1c1a2
mhafiziskandar <mhafiziskandar.dev@gmail.com>
2024-11-17 06:51:58 +0800

    chore: add student panel provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\EducatorPanelProvider::class,
+    App\Providers\Filament\StudentPanelProvider::class,
     App\Providers\OpenAIServiceProvider::class,
 ];
